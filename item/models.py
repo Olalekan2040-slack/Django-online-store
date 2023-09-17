9246d1a4de94ecf3f929e94f47c9c4a2836d0ec1
Olalekan2040-slack <olalekanquadri58@gmail.com>
2023-09-16 22:14:24 -0800

    new update

--- item/models.py
+++ item/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
+
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
--- puddle/urls.py
+++ puddle/urls.py
@@ -17,12 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 
 from core.views import index, contact
 
-urlpatterns = [
+
+urlpatterns = [ 
     path('contact/', contact, name='contact'),
+    path('items/', include('item.urls')),
     path('', index, name='index'), 
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
