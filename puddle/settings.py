0bcab85aef0218847ad66e53fe1163ac83723bcb
Olalekan2040-slack <olalekanquadri58@gmail.com>
2023-11-10 23:47:08 -0800

    testing

--- puddle/settings.py
+++ puddle/settings.py
@@ -91,6 +91,10 @@ DATABASES = {
         'PASSWORD': 'QB43i2AgCHNavz57JFlkPWfQz1fM3drR',
         'HOST': 'dpg-cl0319bjdq6s73bj6ghg-a.oregon-postgres.render.com', 
         'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',  # or 'verify-full' for more strict verification
+            'sslrootcert': 'postgres://todo_list_fnws_user:QB43i2AgCHNavz57JFlkPWfQz1fM3drR@dpg-cl0319bjdq6s73bj6ghg-a.oregon-postgres.render.com/todo_list_fnws',
+    }
     }
 }
 
