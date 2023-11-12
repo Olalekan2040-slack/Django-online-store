b88ac8d2051a5947baf9a7827a02740faa28b791
Olalekan2040-slack <olalekanquadri58@gmail.com>
2023-11-12 08:10:17 -0800

    modified the numbers of newest items to be displayed

--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ from .forms import SignupForm
 from item.models import Category, Item
 
 def index(request):
-    items = Item.objects.filter(is_sold=False)[0:12]
+    items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
 
 
